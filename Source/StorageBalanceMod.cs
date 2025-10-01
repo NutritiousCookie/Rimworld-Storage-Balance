c387fd1a0c987bbd09b0dab19f953086a8aab138
NutritiousCookie <nutritiouscookie@gmail.com>
2025-10-01 16:35:52 -0500

    split class files, build custom def and patchoperation for research projects

---
+++ Source/StorageBalanceMod.cs
@@ -0,0 +1,86 @@
+﻿using System.Xml;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace StorageBalance
+{    
+    public class ResearchLevelDef : Def
+    {
+        public ResearchProjectDef targetDef;
+    }
+    // PatchOperation to replace research dynamically
+    public class PatchOperationStorageResearch : PatchOperation
+    {
+        public string xpath;
+        public string techLevel;
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            // Build the xpath dynamically
+            XmlNode techPath = xml.SelectSingleNode($"Defs/StorageBalance.ResearchLevelDef[defName=\"{techLevel}\"]/targetDef");
+            XmlNodeList nodes = xml.SelectNodes($"{xpath}/research");
+            if (techPath != null && nodes[0] != null)
+            {
+                foreach (XmlNode node in nodes)
+                {
+                    node.InnerText = $"<li>{techPath.InnerText}</li>";
+                }
+                return true;
+            }
+            else return false;
+        }
+    }
+
+    //[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public class StorageBalanceMod : Mod
+    {
+        // Mod Name
+        public override string SettingsCategory() => "StorageBalanceModName".Translate();
+
+
+        // Settings
+        public static StorageBalanceSettings settings;
+        public static string[] researchTypes = { "Furniture", "Storage" };
+        public StorageBalanceMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<StorageBalanceSettings>();
+        }
+        // Settings GUI
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Rect rectLeftColumn = inRect.LeftPart(0.46f).Rounded();
+            Rect rectRightColumn = inRect.RightPart(0.46f).Rounded();
+
+            Listing_Standard list = new Listing_Standard();
+            list.Begin(inRect);
+            list.CheckboxLabeled("StorageBalanceDoResearchLabel".Translate(), ref settings.doResearch, null);
+            list.End();
+
+            Listing_Standard listLeft = new Listing_Standard();
+            listLeft.ColumnWidth = rectLeftColumn.width;
+            listLeft.Begin(rectLeftColumn);
+            if (settings.doResearch)
+            {
+                listLeft.Gap();
+                listLeft.Gap();
+                listLeft.Label("StorageBalanceResearchTypeLabel".Translate());
+                listLeft.Gap();
+                foreach (var option in researchTypes)
+                {
+                    if (listLeft.RadioButton((option + "ResearchTypeLabel").Translate(), settings.researchType == option, 0f, (option + "ResearchTypeDescription").Translate(), 0.1f))
+                    {
+                        settings.researchType = option;
+                    }
+                }
+            }
+            listLeft.End();
+
+            Listing_Standard listRight = new Listing_Standard();
+            listRight.ColumnWidth = rectRightColumn.width;
+            listRight.Begin(rectRightColumn);
+            listRight.End();
+
+            settings.Write();
+        }
+    }
+}
--- Source/StorageBalanceSettings.cs
+++ Source/StorageBalanceSettings.cs
@@ -1,80 +1,18 @@
 ﻿using Verse;
-using UnityEngine;
-using RimWorld;
 
 namespace StorageBalance
 {
     public class StorageBalanceSettings : ModSettings
     {
         public bool doResearch;
-        public string researchTree;
+        public string researchType;
+        /// public string researchUltra;
 
-        /// <summary>
-        /// The part that writes our settings to file. Note that saving is by ref.
-        /// </summary>
         public override void ExposeData()
         {
-            Scribe_Values.Look(ref doResearch, "DoStorageResearch", true);
-            Scribe_Values.Look(ref researchTree, "StorageResearchProjects", "Furniture");
+            Scribe_Values.Look(ref doResearch, "doStorageResearch");
+            Scribe_Values.Look(ref researchType, "researchType", "Furniture");
             base.ExposeData();
-        }
-    }
-    public class StorageBalanceMod : Mod
-    {
-        /// <summary>
-        /// Mod Name
-        /// </summary>
-        public override string SettingsCategory() => "StorageBalanceModName".Translate();
-        /// <summary>
-        /// Settings
-        /// </summary>
-        public static StorageBalanceSettings settings;
-        public static string[] researchTrees = { "Furniture", "Storage" };
-        public StorageBalanceMod(ModContentPack content) : base(content)
-        {
-            settings = GetSettings<StorageBalanceSettings>();
-        }
-
-        /// <summary>
-        /// Settings GUI
-        /// </summary>
-        /// <param name="inRect">A Unity Rect with the size of the settings window.</param>
-        public override void DoSettingsWindowContents(Rect inRect)
-        {
-            Rect rectLeftColumn = inRect.LeftPart(0.46f).Rounded();
-            Rect rectRightColumn = inRect.RightPart(0.46f).Rounded();
-
-            Listing_Standard listLeft = new Listing_Standard();
-            listLeft.ColumnWidth = rectLeftColumn.width;
-            listLeft.Begin(rectLeftColumn);
-
-            listLeft.CheckboxLabeled("StorageBalanceDoResearchLabel".Translate(), ref settings.doResearch, null);
-
-            if (settings.doResearch)
-            {
-                listLeft.Gap();
-                listLeft.Gap();
-                listLeft.Label("StorageBalanceResearchTypeLabel".Translate());
-                listLeft.Gap();
-                foreach (var option in researchTrees)
-                {
-                    if (listLeft.RadioButton((option + "ResearchTypeLabel").Translate(), settings.researchTree == option, 0f, (option + "ResearchTypeDescription").Translate(), 0.1f))
-                    {
-                        settings.researchTree = option;
-                    }
-                }
-            }
-
-            listLeft.End();
-
-
-            Listing_Standard listRight = new Listing_Standard();
-            listRight.ColumnWidth = rectRightColumn.width;
-            listRight.Begin(rectRightColumn);
-
-            listRight.End();
-
-            settings.Write();
         }
     }
 }
